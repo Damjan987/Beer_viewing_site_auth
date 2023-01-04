a5f787f3a574fd10be156938ec4431445e13c85b
Damjan987 <jasamprogramer7@gmail.com>
2023-01-04 12:46:35 +0100

    init

---
+++ beer_viewing_site_auth/index.js
@@ -0,0 +1,18 @@
+const express = require("express");
+const app = express();
+const cors = require("cors");
+const morgan = require("morgan");
+const connectDb = require("./database/db");
+const authRoutes = require("./routes/auth");
+
+// middleware
+app.use(cors());
+app.use(morgan("dev"));
+app.use(express.json());
+app.use('/api/auth', authRoutes);
+
+connectDb();
+
+const port = process.env.PORT || 5000;
+
+app.listen(port, () => console.log(`Listening on port ${port}`));
